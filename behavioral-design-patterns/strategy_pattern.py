eed259c0cd0328bf658ea9e4ead6653ccf1e3519
vahid <vahidncoder@gmail.com>
2024-12-24 23:01:29 +0330

    Added more Behavioral Patterns

--- behavioral-design-patterns/strategy_pattern.py
+++ behavioral-design-patterns/strategy_pattern.py
@@ -2,29 +2,24 @@ from abc import ABC, abstractmethod
 import random
 
 
-class SortStrategy(ABC):
+class SortAlgorithm(ABC):
     @abstractmethod
     def sort(self, data: list[int]) -> list[int]:
         pass
 
 
-class BubbleSortStrategy(SortStrategy):
+class BubbleSortAlgorithm(SortAlgorithm):
     def sort(self, data: list[int]) -> list[int]:
         length = len(data)
-        # Outer loop: Repeat the process for each element in the list
         for i in range(length):
-            # Inner loop: Compare adjacent elements
             for j in range(0, length - i - 1):
-                # If the current element is greater than the next:
                 if data[j] > data[j + 1]:
-                    # then, swap them.
                     data[j], data[j + 1] = data[j + 1], data[j]
         return data
 
 
-class QuickSortStrategy(SortStrategy):
+class QuickSortAlgorithm(SortAlgorithm):
     def sort(self, data: list[int]) -> list[int]:
-        # If the list has 1 or no elements, it's already sorted.
         if len(data) <= 1:
             return data
         else:
@@ -35,17 +30,53 @@ class QuickSortStrategy(SortStrategy):
             return self.sort(smaller) + [pivot] + self.sort(greater)
 
 
+class SelectionSortAlgorithm(SortAlgorithm):
+    def sort(self, data: list[int]) -> list[int]:
+        length = len(data)
+        for i in range(length - 1):
+            min_index = i
+            for j in range(i + 1, length):
+                if data[j] < data[min_index]:
+                    min_index = j
+            data[i], data[min_index] = data[min_index], data[i]
+        return data
+
+
+class InsertionSortAlgorithm(SortAlgorithm):
+    def sort(self, data):
+        length = len(data)
+        for i in range(1, length):
+            insert_index = i
+            current_value = data[i]
+            for j in range(i-1, -1, -1):
+                if data[j] > current_value:
+                    data[j+1] = data[j]
+                    insert_index = j
+                else:
+                    break
+            data[insert_index] = current_value
+        return data
+
+
 class Context:
-    def execute(self, data: list[int], strategy: SortStrategy) -> list[int]:
+    def execute(self, data: list[int], algorithm: SortAlgorithm) -> list[int]:
         data = [(item * 2) + random.randint(-10, 10) for item in data]
 
-        return strategy.sort(data)
+        return algorithm.sort(data)
+
 
 # Example of usage
 def main() -> None:
     context = Context()
-    print(context.execute([1, 5, 3, 4, 2], BubbleSortStrategy()))
-    print(context.execute([1, 5, 3, 4, 2], QuickSortStrategy()))
+    print('Sorted by (Bubble algorithm): ',
+          context.execute(
+              [1, 5, 3, 4, 2], BubbleSortAlgorithm()))
+    print('Sorted by (Quick algorithm): ', context.execute(
+        [1, 5, 3, 4, 2], QuickSortAlgorithm()))
+    print('Sorted by (Selection algorithm): ', context.execute(
+        [1, 5, 3, 4, 2], SelectionSortAlgorithm()))
+    print('Sorted by (Insertion algorithm): ', context.execute(
+        [1, 5, 3, 4, 2], InsertionSortAlgorithm()))
 
 
 if __name__ == "__main__":
